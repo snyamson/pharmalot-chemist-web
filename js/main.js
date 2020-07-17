43c6a09a452254f2a1646bde6eee721689d205da
snyamson <snyamson@st.knust.edu.gh>
2020-07-17 12:33:28 +0000

    complete desktop version of homepage

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+
+//   Made with <i class="fa fa-heart-o" aria-hidden="true"></i> in <a href="" target="_blank">Ghana</a>
+//   © 2020 Copyright | Pharmalot ChemitText
