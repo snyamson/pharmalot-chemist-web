b5e02730928f68af736001c14eff50251aede7bc
snyamson <snyamson@st.knust.edu.gh>
2020-07-18 12:33:08 +0000

    start mart embed

--- js/main.js
+++ js/main.js
@@ -4,10 +4,35 @@ $(document).ready(function () {
 });
 
 // CAROUSEL TRIGGER
-$(".carousel.carousel-slider").carousel({
-  fullWidth: true,
-  indicators: true,
-});
+// $(".carousel.carousel-slider").carousel({
+//   fullWidth: true,
+//   indicators: true,
+// });
 
 // AOS
 AOS.init();
+
+// $('.carousel.carousel-slider').carousel({
+//     fullWidth: true,
+//     indicators: true,
+//     padding: 200
+//   }, setTimeout(autoplay, 4500));
+
+//   function autoplay() {
+//     $('.carousel').carousel('next');
+//     setTimeout(autoplay, 4500);
+//   }
+
+$(".carousel.carousel-slider").carousel({ fullWidth: true, indicators: true });
+var autoplay = true;
+setInterval(function () {
+  if (autoplay) $(".carousel.carousel-slider").carousel("next");
+}, 4500);
+$(".carousel.carousel-slider").hover(
+  function () {
+    autoplay = false;
+  },
+  function () {
+    autoplay = true;
+  }
+);
